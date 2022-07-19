7918bf0bc74bdac01e633747109619f9459512ea
Bogdan <bo.bodnaruk@gmail.com>
2022-07-19 22:16:37 +0300

    first app

--- js/new-script.js
+++ js/new-script.js
@@ -1,3 +1,18 @@
 "use sctrict";
 
-let number = 5;
+ const numberOfFilms = prompt('How much films you viewed?', '');
+
+ const personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    private : false
+ };
+
+ const lastViewedFilm = prompt('You last viewed film?', '');
+ const reitFilm = prompt('How much you rating him?', '');
+
+ personalMovieDB[lastViewedFilm] = reitFilm;
+
+ console.log(personalMovieDB);
